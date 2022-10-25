e2c0b6d76fddfbcb8d14ba5504611e8af171d385
Ridwan Halim <190102hr@gmail.com>
2022-10-25 22:59:35 +0700

    tes

--- lib/views/apps_pages/akun.dart
+++ lib/views/apps_pages/akun.dart
@@ -59,7 +59,7 @@ class _MyAkunState extends State<MyAkun> {
                 setState(() {});
               },
               child: const Text(
-                'Galerry',
+                'Galery',
               ),
             ),
             Container(
